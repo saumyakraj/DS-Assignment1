0f7d27e0645343d4e240927092990d601a8789fd
Saumyak Raj <rajsaumyak@gmail.com>
2023-02-05 14:10:44 +0530

    removed error files

--- PartB/dis_queue/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Producer, Consumer, Message
-
-# Register your models here.
-admin.site.register(Topic)
-admin.site.register(Producer)
-admin.site.register(Consumer)
-admin.site.register(Message)
--- PartB/dis_queue/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DisQueueConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dis_queue'
